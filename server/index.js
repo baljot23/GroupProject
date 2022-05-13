bade4b699aa38e8a964dce68ed63cfa20d7de6e0
baljot23 <92813636+baljot23@users.noreply.github.com>
2022-05-13 02:23:40 -0400

    Hello

--- server/index.js
+++ server/index.js
@@ -2,6 +2,10 @@
 
 const express = require("express");
 const morgan = require("morgan");
+const cors = require("cors");
+const helmet = require("helmet");
+const path = require("path");
+const compression = require("compression");
 
 const {
   getItems,
@@ -10,7 +14,7 @@ const {
   getFilteredItems,
 } = require("./handlers");
 
-const PORT = 4000;
+const PORT = 8080;
 
 express()
   .use(function (req, res, next) {
@@ -25,16 +29,32 @@ express()
     next();
   })
   .use(morgan("tiny"))
-  .use(express.static("./server/assets"))
-  .use(express.json())
-  .use(express.urlencoded({ extended: false }))
-  .use("/", express.static(__dirname + "/"))
+  .use(
+    helmet({
+      contentSecurityPolicy: false,
+    })
+  );
+app.use(cors()).use(express.static("./server/assets"));
+app.use(express.json({ limit: "25mb" }));
+app.use(express.urlencoded({ extended: false, limit: "25mb" }));
+if (process.env.NODE_ENV === "developement") {
+  app.use("/", express.static(__dirname + "/"));
+}
 
-  // REST endpoints?
+// REST endpoints?
+app
   .get("/bacon", (req, res) => res.status(200).json("🥓"))
   .get("/api/get-items", getItems)
   .get("/api/get-items/:_id", getItem)
   .patch("/api/place-order", placeOrder)
-  .get("/api/get-filtered-items", getFilteredItems)
+  .get("/api/get-filtered-items", getFilteredItems);
 
-  .listen(PORT, () => console.info(`Listening on port ${PORT}`));
+// The section below is to serve React on heroku server
+if (process.env.NODE_ENV === "production") {
+  // Serve any static files
+  app.use(express.static(path.resolve(__dirname, "../client/build")));
+  // Handle React routing, return all requests to React app  app.get("*", function (req, res) {
+  res.sendFile(path.resolve(__dirname, "../client/build", "index.html"));
+}
+
+app.listen(PORT, () => console.info(`Listening on port ${PORT}`));
